bf5406b00c8e64b925d25e3e915b46815647bfbb
DandelionQZ <155097020+DandelionQZ@users.noreply.github.com>
2026-05-13 11:40:50 +0900

    [Passed] 특정 위치의 문자 (68ms, 10MB)

---
+++ trail1/특정 위치의 문자/char-in-specific-location.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().trim()
+const arr = ['L', 'E', 'B', 'R', 'O', 'S']
+
+let idx = -1
+
+for (let i = 0; i<6; i++) {
+    if (arr[i] === input) {
+        idx = i
+    }
+}
+
+if (idx === -1) {
+    console.log('None')
+} else {
+    console.log(idx)
+}
